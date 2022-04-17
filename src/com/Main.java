072f9b1c5cf1f48b704c3c30486c642beb339987
RomanV521 <romav5166@gmail.com>
2022-04-17 15:18:21 +0300

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,29 @@
+package com;
+
+
+/**
+ * Vykhodets Roman, #1
+ */
+public class Main {
+
+    private static void Start() {
+        System.out.println("\nCompleted by student AT-212 \nOdessa Polytechnic University \nVykhodets Roman \n\nVariant 1 \n\n");
+    }
+
+    /**
+     * Поля класса  |                   Задача
+     * -------------|------------------------------------------------
+     * Продавец     |   Определить количество товаров, проданных
+     * Наименование |   продавцом «Иванов», вывести сведения о них и
+     * Количество   |   определить товар с максимальной стоимостью.
+     * Цена         |
+     * Дата продажи |
+     */
+    private static void task1(){
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
